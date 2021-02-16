b7e4965e7cbf79e5c9b612b1f0e3965e62e09df4
Super-Web112 <74626440+Super-Web112@users.noreply.github.com>
2021-02-16 01:26:04 -0800

    Updated_titile
    this is changed the title by KMW

--- resources/views/layout/temp.php
+++ resources/views/layout/temp.php
@@ -3,7 +3,7 @@
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h4 class="modal-title">Register</h4>
+              <h4 class="modal-title">Registers</h4>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
