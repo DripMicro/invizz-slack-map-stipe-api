9a0f2289bffe827f361fd55b52e95e6293705286
Super-Web112 <74626440+Super-Web112@users.noreply.github.com>
2021-02-21 14:13:43 -0800

    completion_without_slack

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -259,7 +259,7 @@
                                     </div> <!-- end col -->
                                 </div> <!-- end row -->
 
-                                <div class="text-right">
+                                <div class="text-right" style="display:flex;justify-content:space-between;">
                                     <button type="button" class="btn btn-success waves-effect waves-light mt-2" id="save_profile"><i class="mdi mdi-content-save"></i> Save</button>
                                     <button type="button" class="btn btn-danger waves-effect waves-light mt-2" id="deactive_account"><span class="iconify" data-icon="fe:disabled" data-inline="false"></span> Deactivate account</button>
                                 </div>
